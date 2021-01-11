483371f8ef4c0306b109efc91a26eb5f381a7856
wuaoxiang <pylint@yandex.com>
2021-01-11 14:37:20 +0800

    Add ffi bind C const int example

--- rust_call_c_dll_sll/src/dll.c
+++ rust_call_c_dll_sll/src/dll.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+const int three = 3;
+
 void print_from_dynamic_linking_library() {
     printf("call %s() from %s\n", __FUNCTION__, __FILE__);
 }
--- rust_call_c_dll_sll/src/main.rs
+++ rust_call_c_dll_sll/src/main.rs
@@ -1,12 +1,19 @@
 extern "C" {
-    /// function prototype defined in src/dll.c
-    fn print_from_dynamic_linking_library();
-    /// function prototype defined in src/sll.c
+    /// src/sll.c
     fn print_from_static_linking_library();
+}
+
+/// src/dll.c
+#[link(name="dll", kind="dylib")]
+extern "C" {
+    pub static three: i32;
+    fn print_from_dynamic_linking_library();
 }
 
 fn main() {
     unsafe {
+        dbg!(three);
+        assert_eq!(three, 3);
         print_from_dynamic_linking_library();
         print_from_static_linking_library();
     }
