44221760bf6839aaffc31de4e6ade1216c3bf4a5
w <os.popen@gmail.com>
2021-03-31 14:36:47 +0800

    update statically_linked_executable

--- static_linking_multi_files/statically_linked_executable.s
+++ static_linking_multi_files/statically_linked_executable.s
@@ -1,17 +1,22 @@
-global _start
+# target: linux_x86_64, syntax: AT&T
+# gcc -c statically_linked_executable.s && ld statically_linked_executable.o && ./a.out
+# or
+# as statically_linked_executable.s && ld statically_linked_executable.o && ./a.out # GNU assembler (GNU Binutils)
+# or use gcc -nostdlib compile c code(main->_start) get statically linked binary
+# echo "int _start(){return 0;}" > main.c && gcc -nostdlib main.c && ldd ./a.out
 
-section .text
+    .global _start
+    .text
 _start:
-    mov rdi, 1   ; stdout fd
-    mov rsi, msg
-    mov rdx, 9   ; 8 chars + newline
-    mov rax, 1   ; write syscall
-    syscall
-
-    xor rdi, rdi ; return code 0
-    mov rax, 60  ; exit syscall
-    syscall
-
-section .data
-msg:
-    db "hi there", 10 ; "hi there\n", b'\n'=10u8
+    # write(1, message, 13)
+    mov     $1, %rax                # system call 1 is write
+    mov     $1, %rdi                # file_descriptor 1 is stdout
+    mov     $message, %rsi          # address of string to output
+    mov     $13, %rdx               # len of string
+    syscall                         # invoke operating system to do the write
+    # exit(0)
+    mov     $60, %rax               # system call 60 is exit
+    xor     %rdi, %rdi              # we want return code 0
+    syscall                         # invoke operating system to exit
+message:
+    .ascii  "Hello, world\n"
